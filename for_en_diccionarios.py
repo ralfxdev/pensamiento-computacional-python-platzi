b29bf223771c9b8eff67cfd98ed097ebcfbec5fc
ralfxdev <lopezalexander1019@gmail.com>
2023-03-13 22:42:23 -0600

    pensamiento_computacional_con_python

---
+++ for_en_diccionarios.py
@@ -0,0 +1,8 @@
+estudiantes = {
+    'mexico': 10,
+    'colombia': 15,
+    'puerto_rico': 4,
+}
+
+for pais, numero_de_estudiantes in estudiantes.items():
+    print(pais, numero_de_estudiantes)
---
+++ programas_ramificados.py
@@ -0,0 +1,9 @@
+num_1 = int(input('Ingresa un entero: '))
+num_2 = int(input('Ingresa otro entero: '))
+
+if num_1 > num_2:
+    print('El primer numero es mayor que el segundo')
+elif num_1 < num_2:
+    print('El segundo numero es mayor que el primero')
+else:
+    print('Los dos numeros son iguales')
